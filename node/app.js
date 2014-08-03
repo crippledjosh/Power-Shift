1386539773aa5ac5120eb4610dea17675aeba1bd
Joshua Marks <jmarks.joshua@googlemail.com>
2014-08-03 17:49:41 +0100

    removed all of the unrequired code that was legacy from final year project

--- node/app.js
+++ node/app.js
@@ -7,18 +7,6 @@ server = http.createServer(app);
 app.set('view engine', 'jade');
 
 app.use(express.bodyParser());
-app.use(express.cookieParser('Sometimes I pretend I"m a car.'));
-app.use(express.session());
-app.use(function(req, res, next){
-	var err = req.session.error,
-	msg = req.session.success;
-	delete req.session.error;
-	delete req.session.success;
-	res.locals.message = '';
-	if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
-	if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
-	next();
-});
 
 require('routes')(server, app);
 
