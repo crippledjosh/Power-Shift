eeebab48850a248e18a900a54f354d86ea8563ff
Joshua Marks <jmarks.joshua@googlemail.com>
2014-08-03 17:35:44 +0100

    Have removed weather.html to reduce duplicate html definitions everything is now created through template

--- node/app.js
+++ node/app.js
@@ -1,5 +1,26 @@
-var server = require('routes')();
+var express = require('express'),
+	app = express(),
+	http = require('http');
 
+server = http.createServer(app);
+
+app.set('view engine', 'jade');
+
+app.use(express.bodyParser());
+app.use(express.cookieParser('Sometimes I pretend I"m a car.'));
+app.use(express.session());
+app.use(function(req, res, next){
+	var err = req.session.error,
+	msg = req.session.success;
+	delete req.session.error;
+	delete req.session.success;
+	res.locals.message = '';
+	if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
+	if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
+	next();
+});
+
+require('routes')(server, app);
 
 
 server.listen(8080);
